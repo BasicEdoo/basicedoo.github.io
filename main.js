055259e01ca46771f739dd5358f036249433de02
BasicEdoo <edin.b@hotmail.com>
2022-10-26 14:49:13 +0200

    poke
    poke api

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function dropDown() {
       x.style.display = "block";
     }
   }
+
+async function getText(file) {
+    let x = await fetch(file);
+    let y = await x.text();
+    document.getElementById("demo").innerHTML = y;
+}
+getText("https://pokeapi.co/api/v2/pokemon/");
+
+
